791e396b0356a7571146a31770656b452ec153cc
FeDorDrolma98 <58807436+FeDorDrolma98@users.noreply.github.com>
2022-09-27 21:50:00 +0200

    Fin scéance TD3

--- import-data.js
+++ import-data.js
@@ -1,11 +1,39 @@
 require('dotenv').config()
 
+// Test ecriture promesses et connection--------------
+const mongoosy={connect:(param)=>new Promise((resolve,reject)=>setTimeout(()=>resolve('Connected'+param),Math.random()*3600))}
+async function main (){
+    //ca enrobe la fonction dans une promesse, reussi ou echoue
+    const results=await Promise.all([mongoosy.connect(1),mongoosy.connect(2),mongoosy.connect(3)])
+    console.log(results)// la fait tableau avec tout, du coup penalisé par le plus lent
+    const speed=await Promise.race([mongoosy.connect(1),mongoosy.connect(2),mongoosy.connect(3)])
+    console.log(speed)// la mets que le plus rapide
+    const result=await mongoosy.connect(1)
+    console.log(result)
+    const result2=await mongoosy.connect(2)
+    console.log(result2)
+    const result3=await mongoosy.connect(3)
+    console.log(result3)//on attend que chaque code soit fini avant la suite (cf await)
+}
+//console.log(main())//-------------------------------
+
+
 //console.log(process.env.MONGO_URI);
-
 const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI).then((res)=>console.log('connecté')).catch((e)=>console.log(e));
 
-const Locations=mongoose.Schema({//on fait un template en fait
+//on fait un doc a coté pour éviter de rentrer le mdp en dur
+//ici, MONGO_URI a password dedans, on le remplace par notre mdp
+
+//en fait mongoose.connect, c'est une fonction assychrone, c'est une promesse.
+// quand s'execute, on a aucune preuve que ca ai marché
+// les fonction promesse, soit elles reussissent (appelle .then), soit elle echoue (appelle .catch)
+//la on connecte, puis on met .then(et la truc entre parenthese qu'il va faire)
+//.then((res)=>trucsicamarche.catch((e)=>trucsicamarchepas) -->on a ecrit res, mais result ca marche aussi
+//la trucsicamarchepas:console.log(e), mais on aurai pu juste ecrire n'importe quoi
+
+
+const LocationSchema=mongoose.Schema({//on fait un template en fait
     "filmType": String,
     "filmProducerName": String,
     "endDate": Date,//avant : {type:Date,default:Date.now}, mais mieux que pas de default, mieux que pas d'infos plutot que de fausses infos
@@ -18,3 +46,46 @@ const Locations=mongoose.Schema({//on fait un template en fait
     "startDate": Date,//avant : {type:Date,default:Date.now}, mais mieux que pas de default, mieux que pas d'infos plutot que de fausses infos
     "year": String,//Number possible aussi
 })
+const Location=mongoose.model('Location',LocationSchema,'locations');
+//module.exports=mongoose.model("Locations",locationMod);
+const filmingLocations=require('./lieux-de-tournage-a-paris.json');//faut le mettre dans le dossier avant of course
+
+
+function populate(){
+    for(const elem of filmingLocations){
+        const locat=new Location({
+            "filmType": elem['fields']['type_tournage'],
+            "filmProducerName": elem['fields']['nom_producteur'],
+            "endDate": elem['fields']['date_fin'],
+            "filmName": elem['fields']['nom_tournage'],
+            "district": elem['fields']['ardt_lieu'],
+            "geolocation": elem['fields']['geometry'],
+            "sourceLocationId": elem['fields']['id_lieu'],
+            "filmDirectorName": elem['fields']['nom_realisateur'],
+            "address": elem['fields']['adresse_lieu'],
+            "startDate": elem['fields']['date_debut'],
+            "year": elem['fields']['annee_tournage'],
+        });
+        locat.save().then((res)=>console.log('enregistré !')).catch((e)=>console.log(e));
+    }
+}
+
+async function populate2(){
+    //si on fait pas wait sur le .save, ca va tous les lancer d'un coup
+    //si un fail, tu saura pas lequel
+
+    let promisesChunk=[];
+    for(const filmingLocation of filmingLocations){
+        const toInsert=new Model({})
+        promisesChunk.push(toInsert.save())//la on attend pas, on lance toutes les insertions
+        if(promisesChunk.length===500){
+            await Promise.all(promisesChunk)//car si vrmt bcp prends du temps
+            promisesChunk=[]
+        }
+    }
+    const results=await Promise.all(promisesChunk)//la on attend que toutes les insertions se finissent, et du coup
+}
+
+function queryOneLoca(id){
+
+}
